73a4a86d898b4b7fdc3632d5b66b920621a20144
CaoHuy26 <huyc197@gmail.com>
2021-09-19 06:38:39 +0700

    feat: buy token when it is added

--- index.ts
+++ index.ts
@@ -1,17 +1,20 @@
 
 import 'dotenv/config'
 import { Transaction } from 'web3-eth'
-import { PANCAKE_ROUTER } from './src/constants/constants'
+import { ADDRESS_ZERO, PANCAKE_ROUTER, WBNB } from './src/constants/constants'
+import buyToken from './src/helpers/exchangeToken'
 import decoder from './src/utils/decoder'
+import { getPair } from './src/utils/liquidity'
 import getPath from './src/utils/path'
 import getWeb3 from './src/utils/web3'
 
-const {
-  TARGET_TOKEN
-} = process.env
+const sender = process.env.SENDER as string
+const targetToken = process.env.TARGET_TOKEN as string
+const amountBNB = Number(process.env.AMOUNT_BNB)
 
 const main = async () => {
   const web3 = getWeb3()
+  const path = getPath(targetToken)
   
   web3.eth.subscribe('pendingTransactions')
     .on('connected', () => console.log('Connected'))
@@ -19,16 +22,19 @@ const main = async () => {
       const tx: Transaction = await web3.eth.getTransaction(txHash)
       
       if (tx?.to === PANCAKE_ROUTER) {
+        const gasPrice = Number(tx?.gasPrice) - .05 * Number(tx?.gasPrice)
         const txInputDecoded = decoder(tx.input)
 
         if (txInputDecoded?.name === 'addLiquidityETH') {
           const [token] = txInputDecoded.params
-          const path = getPath(TARGET_TOKEN as string)
+          const pair = await getPair(token?.value as string, WBNB)
 
-          if (path.includes(token?.value)) {
+          if (path.includes(token?.value) && pair === ADDRESS_ZERO) {
+            console.log(tx.hash)
             console.log(txInputDecoded)
 
-            // Buy token
+            const result = await buyToken(sender, path, gasPrice, amountBNB)
+            console.log('Buy success: ' + result)
           }
         }
       }
--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -1,9 +1,8 @@
 import 'dotenv/config'
 import Web3 from 'web3'
 
-const provider = process.env.BSC_WSS as string
-
 const getWeb3 = () => {
+  const provider = process.env.BSC_WSS as string
   return new Web3(new Web3.providers.WebsocketProvider(provider, {
     reconnect: {
       auto: true,
@@ -12,6 +11,12 @@ const getWeb3 = () => {
       onTimeout: false
     }
   }))
+
+}
+
+export const getWeb3HTTP = () => {
+  const provider = process.env.BSC_HTTP as string
+  return new Web3(new Web3.providers.HttpProvider(provider))
 }
 
 export default getWeb3
